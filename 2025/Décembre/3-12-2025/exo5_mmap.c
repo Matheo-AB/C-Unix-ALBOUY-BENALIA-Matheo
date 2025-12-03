7360b9dce0a32f9ff82ae3b2bd462adcdea336b7
ALBOUY-BENALIA <albouy-m@3il.fr>
2025-12-03 16:50:33 +0100

    Add : Ajout de l'exercice N°5

--- 2025/Décembre/3-12-2025/exo5_mmap.c
+++ 2025/Décembre/3-12-2025/exo5_mmap.c
@@ -4,11 +4,10 @@
 #include <sys/wait.h>
 #include <sys/mman.h>
 
-
-
 int main()
     {
         int status;
+        pid_t pid;
 
         int *compteur = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
         if (compteur == MAP_FAILED)
@@ -18,15 +17,32 @@ int main()
             }
         *compteur = 0;
 
-        if(fork == 0)
+    /**    int *compteur2 = mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+        if (compteur2 == MAP_FAILED)
             {
-                for(; *compteur < 10 ; *compteur++)
+                perror("mmap");
+                exit(EXIT_FAILURE);
+            }
+        *compteur2 = 0;*/
+
+        if(pid == 0)
+            {
+                for(int i=0; i < 10 ; i++)
                     {
-                        *compteur++;
+                        (*compteur)++;
                         printf("\n Le compteur Fils est : %d",*compteur);
+                        fflush(stdout); //Forcer l'affichage
                         sleep(1);
                     }
+                exit(0);
+                printf("La valeur finale du compteur père est : %d\n",*compteur);
             }
-
+        while(fork == 0)
+            {
+                printf("\n Le compteur Père est : %d",*compteur);
+                wait(NULL);
+            }
+        printf("La valeur finale du compteur père est : %d\n",*compteur);
         return 0;
+        munmap(compteur, sizeof(int));
     }
